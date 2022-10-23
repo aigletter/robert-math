219c26cd2e70bb61c3cb454076eebea6e3cbfc4d
Yurii Orlyk <orlyk.rollun@gmail.com>
2022-10-23 15:05:48 +0000

    init

---
+++ src/Math.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Yurii\Math;
+
+class Math
+{
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function divide($a, $b)
+    {
+        return $a / $b;
+    }
+}
